67685f4987f8987455aa1cc73a03cb8ec5372c62
benoit <benoit.remy@ext.fabernovel.com>
2024-10-09 10:49:50 +0200

    [API] feat: use AWS S3 for uploaded files

--- vagabond-api/config/middlewares.ts
+++ vagabond-api/config/middlewares.ts
@@ -1,12 +1,41 @@
+import { env } from "@strapi/utils";
+
+const fullS3BucketUrl = `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.localhost.localstack.cloud`;
+
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  "strapi::security",
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            `${env("CDN_URL")}/`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            `${env("CDN_URL")}/`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
--- vagabond-api/config/plugins.ts
+++ vagabond-api/config/plugins.ts
@@ -1,5 +1,25 @@
+import { env } from "@strapi/utils";
+
 export default () => ({
-  // "location-plugin": {
-  //   enabled:  false,
-  // },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        baseUrl: env("CDN_URL"),
+        s3Options: {
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
+          region: env("AWS_REGION"),
+          params: {
+            Bucket: env("AWS_BUCKET"),
+          },
+          // For local development only
+          endpoint: env("AWS_PRIVATE_ENDPOINT"),
+          forcePathStyle: env.bool("AWS_S3_FORCE_PATH_STYLE", false),
+        },
+      },
+    },
+  },
 });
