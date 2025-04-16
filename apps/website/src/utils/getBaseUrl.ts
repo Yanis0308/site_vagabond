7679a72f836b0febf6b6ba4538824acf5cc7dbe2
benoit-ctrl <benoit.remylang@gmail.com>
2025-04-16 11:18:51 +0200

    fix: update production base URL with www

--- apps/website/src/utils/getBaseUrl.ts
+++ apps/website/src/utils/getBaseUrl.ts
@@ -1,5 +1,5 @@
 export function getBaseUrl(): string {
   return process.env.VERCEL_ENV === "production"
-    ? `https://vagabond.gg`
+    ? `https://www.vagabond.gg`
     : (process.env.VERCEL_URL ?? `http://localhost:3002`);
 }
