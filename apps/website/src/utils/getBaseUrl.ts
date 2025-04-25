39f583c8fdf278d62793785070991e2f2ea2d9d8
benoit-ctrl <benoit.remylang@gmail.com>
2025-04-25 15:43:59 +0200

    fix: fix getBaseUrl function on client

--- apps/website/src/utils/getBaseUrl.ts
+++ apps/website/src/utils/getBaseUrl.ts
@@ -1,5 +1,5 @@
 export function getBaseUrl(): string {
-  return process.env.VERCEL_ENV === "production"
+  return process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
     ? `https://www.vagabond.gg`
-    : (process.env.VERCEL_URL ?? `http://localhost:3002`);
+    : (process.env.NEXT_PUBLIC_VERCEL_URL ?? `http://localhost:3002`);
 }
