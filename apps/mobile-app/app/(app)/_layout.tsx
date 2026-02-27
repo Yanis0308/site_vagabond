1e94175cc6f59c160501f4013e7fbcc8c1275cd0
benoit-ctrl <benoit.remylang@gmail.com>
2026-02-27 19:27:07 +0100

    fix(VG-103): transparency bug on fade animation samsung android device

--- apps/mobile-app/app/(app)/_layout.tsx
+++ apps/mobile-app/app/(app)/_layout.tsx
@@ -4,27 +4,14 @@ import { type ReactElement } from "react";
 import { defaultScreenOptions } from "@/constants/ScreenOptions";
 
 export default function RootLayout(): ReactElement | null {
+  // We have removed custom chosed fade animation for now
+  // because on a Samsung Android device that provokes transparency problem after place review form
   return (
     <Stack screenOptions={defaultScreenOptions}>
       <Stack.Screen name="(tabs)" />
-      <Stack.Screen
-        name="validate-place/review-form"
-        options={{
-          animation: "fade",
-        }}
-      />
-      <Stack.Screen
-        name="search"
-        options={{
-          animation: "fade",
-        }}
-      />
-      <Stack.Screen
-        name="user/[userId]"
-        options={{
-          animation: "fade",
-        }}
-      />
+      <Stack.Screen name="validate-place/review-form" />
+      <Stack.Screen name="search" />
+      <Stack.Screen name="user/[userId]" />
     </Stack>
   );
 }
