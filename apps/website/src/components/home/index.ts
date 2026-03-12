517aa3e93a7a9b06d9120f232caf45874ec7fcc7
benoit-ctrl <benoit.remylang@gmail.com>
2026-03-12 12:26:21 +0100

    chore: remove old website

--- apps/website/src/components/home/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AppFeatures } from "./AppFeatures";
-export { default as CountdownTimer } from "./CountdownTimer";
-export { default as Hero } from "./Hero";
-export { default as WhatIsVagabond } from "./WhatIsVagabond";
--- apps/website/src/utils/getBaseUrl.ts
+++
@@ -1,5 +0,0 @@
-export function getBaseUrl(): string {
-  return process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? `https://www.vagabond.gg`
-    : (process.env.NEXT_PUBLIC_VERCEL_URL ?? `http://localhost:3002`);
-}
